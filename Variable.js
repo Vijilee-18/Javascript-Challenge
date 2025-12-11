cb7bb133824b3b4f5b80dec9bbd8ac29545f9d45
Vijilee George Kurian <vijileegk2006@gmail.com>
2025-12-11 13:04:13 +0530

    Add files via upload
    Basic Variable

---
+++ Variable.js
@@ -0,0 +1,16 @@
+//Basic Variable and Print Statement
+
+//Using 'var' Keyword
+
+var Name="Vijilee George Kurian";  //Overriding of Value and Variable is Possible.
+console.log(Name); //printing statement 
+
+//Using 'let' Keyword
+
+let age=18; 
+console.log(age);        //Overriding of Value is Possible but not Variable Name.
+
+//using 'const' Keyword
+
+const country="India";
+console.log(country);    //Overriding of Value and Variable Name is Not Possible.
